a8dde9d83fd41c7ed3fb0824197b21da644f2b03
RetrasTT <retrast.keke3h@gmail.com>
2024-03-26 00:15:48 +0700

    add delete message websocket event

--- src/gateways/message.gateway.ts
+++ src/gateways/message.gateway.ts
@@ -13,7 +13,7 @@ export class MessageGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('message')
+  @SubscribeMessage('message_send')
   async sendMessage(
     @MessageBody() data: { accessToken: string; text: string; dialogId: string; userId: string }
   ): Promise<void> {
@@ -25,7 +25,21 @@ export class MessageGateway {
       throw new WsException('Unauthorized');
     }
 
-    this.server.emit('message');
     this.messageService.createMessage({ text: data.text, dialogId: data.dialogId, userId: data.userId });
+    this.server.emit('message_send');
+  }
+
+  @SubscribeMessage('message_delete')
+  async deleteMessage(@MessageBody() data: { accessToken: string; messageId: number }): Promise<void> {
+    try {
+      await this.jwtService.verifyAsync(data.accessToken, {
+        secret: process.env.ACCESS_TOKEN_SECRET_KEY,
+      });
+    } catch (error) {
+      throw new WsException('Unauthorized');
+    }
+
+    this.messageService.deleteMessage(data.messageId);
+    this.server.emit('message_delete');
   }
 }
